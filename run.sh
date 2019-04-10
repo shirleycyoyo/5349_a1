c9c854b45a32416c6c7751d0f3d01bd8273dddd0
shirleycyoyo <38944100+shirleycyoyo@users.noreply.github.com>
2019-04-10 20:26:59 +1000

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir ~/data
+
+
+spark-submit \
+    --master local[2] \
+    Top10Count.py \
+    --input file:///home/hadoop/5349_a1/5349_a1/workload2/lab_commons/a1_data \
+    --output file:///home/hadoop/out/
+	 
+
+    
